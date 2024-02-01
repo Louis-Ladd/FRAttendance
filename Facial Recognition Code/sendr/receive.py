afe3d72f68416dad2a86e934de2f7ce6fa02ea79
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-01 12:52:03 -0700

    PIscript V2

--- Facial Recognition Code/sendr/receive.py
+++ Facial Recognition Code/sendr/receive.py
@@ -1,17 +1,24 @@
 import socket
+import datetime
+import uuid
+import os
+from threading import Thread
 
+face_directory = r"C:\Users\Owner\Desktop\FRAttendance\FRAttedence\Faces"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 1002))
 server.listen()
 
-client_socket, client_address = server.accept()
 
-with open("Proofreceives.png", "wb") as file:
-    image_chunk = client_socket.recv(2048)
+while True:
+    client_socket, client_address = server.accept()
 
-    while image_chunk:
-        file.write(image_chunk)
+    print(f"Recieved image from {client_address[0]}")
+    
+    with open(os.path.join(face_directory, (str(uuid.uuid1()) + ".png")), "wb") as file:
         image_chunk = client_socket.recv(2048)
+        while image_chunk:
+            file.write(image_chunk)
+            image_chunk = client_socket.recv(2048)
 
-client_socket.close()
-server.close()
+    client_socket.close()
--- Facial Recognition Code/sendr/send.py
+++ Facial Recognition Code/sendr/send.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("192.168.50.251", 1002))
+client.connect(("10.235.91.100", 1002))
 
 with open(r"Proof.png", "rb") as file:
     image_data = file.read(2048)
