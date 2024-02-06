48206c47135e9d535f4c815e9d6605e195ec26ac
Fiery51 <tylerjackmo@outlook.com>
2024-02-06 13:16:17 -0700

    haha formatter :)

--- Facial Recognition Code/src/dashboard.py
+++ Facial Recognition Code/src/dashboard.py
@@ -4,35 +4,41 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html", username=request.remote_addr)
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
 
 @app.route("/server")
 def server():
     return render_template("server.html")
 
+
 @app.route("/cameras")
 def camears():
     return render_template("cameras.html")
 
+
 @app.route("/classes")
 def classes():
     return render_template("classes.html")
+
 
 @app.route("/notifications")
 def notifications():
     return render_template("notifications.html")
 
 
-
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
