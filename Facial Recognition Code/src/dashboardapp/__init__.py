36dffd7e8a17771ae92f0510fa97bdc06b409475
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-08 09:47:39 -0700

    Proper project structure

---
+++ Facial Recognition Code/src/dashboardapp/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask 
+
+from .events import socketio
+from .routes import main
+from .updater import updateThread
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+    app.config["SECRET_KEY"] = "secret!"
+
+    app.register_blueprint(main)
+
+    thread = updateThread()
+    thread.start()
+
+    socketio.init_app(app)
+
+    return app
---
+++ Facial Recognition Code/src/dashboardapp/routes.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request
+
+from .updater import server_info
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+    return render_template("index.html", username=request.remote_addr)
+
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@main.route("/server")
+def server():
+    return render_template("server.html", server_info=server_info)
+
+@main.route("/cameras")
+def camears():
+    return render_template("cameras.html")
+
+
+@main.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+
+@main.route("/notifications")
+def notifications():
+    return render_template("notifications.html")
