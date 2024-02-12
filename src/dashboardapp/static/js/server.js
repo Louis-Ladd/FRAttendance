a089cea9b7dfa705d4a732c1b198cf2ed3d33435
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-12 15:31:13 -0700

    Server graphing (graph.js)

--- src/dashboardapp/static/js/server.js
+++ src/dashboardapp/static/js/server.js
@@ -1,5 +1,38 @@
 var socket = io();
-const dynamic_element_ids = ["ip","cpu_usage", "ram_usage"]
+var cpu_usage_history = [];
+const dynamic_element_ids = ["ip","cpu_usage", "ram_usage"];
+
+var cpu_chart = new Chart(document.getElementById("cpu_usage_graph"), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'CPU Usage %',
+            data: [],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+function addData(chart, label, newData) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(newData);
+    });
+    chart.update();
+}
 
 socket.on("connect", function() {
     socket.emit("data", {data: "I\'m connected!"});
@@ -13,6 +46,10 @@ socket.on("info_update", function(data) {
         else{
             document.getElementById(dynamic_element_ids[i]).innerText = data[dynamic_element_ids[i]];
         }
+    }
+    addData(cpu_chart, "", data["cpu_usage"][data["cpu_usage"].length-1])
+    if (cpu_chart.data.datasets[0].data.length > 100) {
+        //TODO: remove first datapoints
     }
     socket.emit("refresh");
 })
