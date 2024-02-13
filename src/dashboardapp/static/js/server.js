c975b5349a8b287f65b152cca38f8ee2e67e2f1f
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-12 23:15:11 -0700

    Server Graphing implemented

--- src/dashboardapp/static/js/server.js
+++ src/dashboardapp/static/js/server.js
@@ -1,13 +1,41 @@
 var socket = io();
 var cpu_usage_history = [];
+var lastGraphUpdate = Date.now()
 const dynamic_element_ids = ["ip","cpu_usage", "ram_usage"];
+const formatter = new Intl.DateTimeFormat('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
+
+//TODO: compact graph institiation code - Louis
 var cpu_chart = new Chart(document.getElementById("cpu_usage_graph"), {
     type: 'line',
     data: {
         labels: [],
         datasets: [{
             label: 'CPU Usage %',
+            data: [],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        elements: {
+            point: {
+                radius: 0
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+var memory_chart = new Chart(document.getElementById("memory_usage_graph"), {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Memory Usage %',
             data: [],
             borderWidth: 1
         }]
@@ -39,6 +67,7 @@ socket.on("connect", function() {
 });
 
 socket.on("info_update", function(data) {
+    const currentDate = new Date();
     for (var i = 0; i < dynamic_element_ids.length; i++){
         if (Array.isArray(data[dynamic_element_ids[i]])){
             document.getElementById(dynamic_element_ids[i]).innerText = data[dynamic_element_ids[i]][data[dynamic_element_ids[i]].length-1];
@@ -47,9 +76,21 @@ socket.on("info_update", function(data) {
             document.getElementById(dynamic_element_ids[i]).innerText = data[dynamic_element_ids[i]];
         }
     }
-    addData(cpu_chart, "", data["cpu_usage"][data["cpu_usage"].length-1])
+
+    //TODO: Make this code less verbose - Louis
+    if ((Date.now() - lastGraphUpdate) > 500) {
+        addData(cpu_chart, formatter.format(currentDate), data["cpu_usage"][data["cpu_usage"].length-1]);
+        addData(memory_chart, formatter.format(currentDate), data["ram_usage"][data["ram_usage"].length-1]);
+        lastGraphUpdate = Date.now();
+    }
+
     if (cpu_chart.data.datasets[0].data.length > 100) {
-        //TODO: remove first datapoints
+        cpu_chart.data.labels.shift();
+        cpu_chart.data.datasets[0].data.shift();
+    }
+    if (memory_chart.data.datasets[0].data.length > 100) {
+        memory_chart.data.labels.shift();
+        memory_chart.data.datasets[0].data.shift();
     }
     socket.emit("refresh");
 })
