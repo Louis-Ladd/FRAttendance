a373a2d662216240822cc7852fb70ceaa9d86564
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-21 17:04:28 -0700

    Login/signup/ authentication added

--- src/dashboardapp/__init__.py
+++ src/dashboardapp/__init__.py
@@ -1,19 +1,47 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
 
 from .events import socketio
-from .routes import main
 from .updater import updateThread
+
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config["DEBUG"] = True
-    app.config["SECRET_KEY"] = "secret!"
+    app.config["SECRET_KEY"] = "secret!" # Please for the love of christ change before production
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    app.register_blueprint(main)
+    db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    print("Starting updateThread...")
     thread = updateThread()
     thread.start()
 
+    print("Starting authentication routes...")
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    print("Starting main routes...")
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    print("Initalizting SocketIO...")
     socketio.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+        
     return app
--- src/dashboardapp/routes.py
+++ src/dashboardapp/routes.py
@@ -1,33 +1,38 @@
 from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 
 from .updater import server_info
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
+@main.route("/home")
+@login_required
 def home():
-    return render_template("index.html", username=request.remote_addr)
-
-@main.route("/login")
-def login():
-    return render_template("login.html")
+    return render_template("index.html", username=current_user.name)
 
 @main.route("/dashboard")
+@login_required
 def dashboard():
     return render_template("dashboard.html")
 
 @main.route("/server")
+@login_required
 def server():
-    return render_template("server.html", server_info=server_info)
+    if current_user.name == "admin":
+        return render_template("server.html", server_info=server_info)
+    return "You do not have permission to view this page"
 
 @main.route("/cameras")
+@login_required
 def camears():
     return render_template("cameras.html")
 
 @main.route("/classes")
+@login_required
 def classes():
     return render_template("classes.html")
 
 @main.route("/notifications")
+@login_required
 def notifications():
     return render_template("notifications.html")
