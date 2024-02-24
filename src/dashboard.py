2b09076f16c7a7d4085f57004cc0fcc72a8b074c
Louis-Ladd <lewisharshman1@gmail.com>
2024-02-24 10:10:04 -0700

    note about HTTPS

--- src/dashboard.py
+++ src/dashboard.py
@@ -4,3 +4,6 @@ app = create_app()
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
+    # This application needs to be self signed to use HTTPS,
+    # I was able to sniff the admin credentials when HTTP is being used.
+    # ssl_context='adhoc'
